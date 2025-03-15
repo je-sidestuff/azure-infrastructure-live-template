2dc3aa6cd93e7b827ddf185cf6c348ee3ba565f8
je-sidestuff <gerald.halimak@gmail.com>
2025-03-15 19:44:05 +0000

    feat: Early draft changes.

--- .smart-init/scripts/populate_templates.sh
+++ .smart-init/scripts/populate_templates.sh
@@ -6,6 +6,33 @@
 #              indicating success.  THIS IS A TEMPLATE SCRIPT AND SHOULD BE
 #              REPLACED BY THE IMPLEMENTER WITH THEIR OWN CUSTOM LOGIC.
 
+
+# We expect the json to look similar to this:
+# {
+#   "targets":{
+#     "storage_account" : {
+#       "repo": "je-sidestuff/terraform-azure-simple-modules",
+#       "path": "modules/data-stores/storage-account/",
+#       "desc": "",
+#       "placement": {
+#         "region": "eastus",
+#         "env": "default",
+#         "subscription": "main"
+#       }
+#     },
+#     "other_storage_account" : {
+#       "repo": "je-sidestuff/terraform-azure-simple-modules",
+#       "path": "modules/data-stores/storage-account/",
+#       "desc": "other",
+#       "placement": {
+#         "region": "eastus",
+#         "env": "_global",
+#         "subscription": "main"
+#       }
+#     }
+#   }
+# }
+
 # Function to print a JSON error message and exit
 print_error() {
   echo "{\"status\": \"error\", \"message\": \"$1\"}"
@@ -14,7 +41,7 @@ print_error() {
 
 # Function to print a JSON success message
 print_success() {
-    echo "{\"status\": \"success\", \"message\": \"File created successfully.\"}"
+  echo "{\"status\": \"success\", \"message\": \"File created successfully.\"}"
 }
 
 # Check if a JSON payload argument is provided
@@ -22,19 +49,58 @@ if [ -z "$1" ]; then
   print_error "No JSON payload provided."
 fi
 
-# Store the JSON payload in a variable
-json_payload="$1"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Extract the filename from the JSON payload.
-filename=$(echo "$json_payload" | jq -r '.filename')
+# Read the JSON payload to feed into the terragrunt scaffolder
+REPO_INIT_PAYLOAD="$1"
 
-# Check if the filename was extracted successfully
-if [ -z "$filename" ]; then
-  print_error "Could not extract filename from JSON."
-fi
+SELF_BOOTSTRAP_SCAFFOLDING="$(echo ${REPO_INIT_PAYLOAD} | jq .self_bootstrap)"
+SELF_BOOTSTRAP_SCAFFOLDING="$(echo ${SELF_BOOTSTRAP_SCAFFOLDING} | jq ".scaffolding_root += \"${SCRIPT_DIR}/../../bootstrap/\"")"
 
-# Create the file (touch creates an empty file)
-touch "$filename"
+export TEMP_DIR="$(mktemp -d -t infra-live-XXXX)"
 
-# Print the success JSON payload
-print_success
+cat << EOF > "${TEMP_DIR}/main.tf"
+module "scaffolding" {
+  source = "git@github.com:je-sidestuff/terraform-github-orchestration.git//modules/terragrunt/scaffolder/from-json/?ref=environment_deployment_support"
+
+  input_json = ${SELF_BOOTSTRAP_SCAFFOLDING}
+}
+EOF
+
+echo "Which terraform?"
+which terraform
+
+sudo apt-get install unzip
+curl "https://releases.hashicorp.com/terraform/1.3.7/terraform_1.3.7_linux_amd64.zip" -o "terraform_1.3.7_linux_amd64.zip"
+unzip terraform_1.3.7_linux_amd64.zip
+sudo mv terraform /usr/local/bin/terraform
+
+echo "Which terraform?"
+which terraform
+
+echo "Which terragrunt?"
+which terragrunt
+
+curl -L https://github.com/gruntwork-io/terragrunt/releases/download/v0.75.10/terragrunt_linux_amd64 -o terragrunt
+chmod +x terragrunt
+sudo mv terragrunt /usr/local/bin/terragrunt
+
+echo "Which terragrunt?"
+which terragrunt
+
+cd "${TEMP_DIR}" && terraform init && terraform plan && cd -
+
+
+# # Extract the filename from the JSON payload.
+# filename=$(echo "$json_payload" | jq -r '.filename')
+
+# # Check if the filename was extracted successfully
+# if [ -z "$filename" ]; then
+#   print_error "Could not extract filename from JSON."
+# fi
+
+# # Create the file (touch creates an empty file)
+# touch "$filename"
+
+# # Print the success JSON payload
+# print_success
