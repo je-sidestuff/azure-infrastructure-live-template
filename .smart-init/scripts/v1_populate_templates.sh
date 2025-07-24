77c622f0489cdad626679631fa23d3499fa0067f
je-sidestuff <gerald.halimak@gmail.com>
2025-07-24 23:11:20 +0000

    Early draft commits.

--- .smart-init/scripts/v1_populate_templates.sh
+++ .smart-init/scripts/v1_populate_templates.sh
@@ -82,6 +82,7 @@ export TGO_REF="scaffold-refactor"
 # Scaffold our scaffolder so it can scaffold the remaining deployment tree
 >&2 mkdir -p $TERRAGRUNT_DEPLOYMENT_SCAFFOLD_DIR
 >&2 cd $TERRAGRUNT_DEPLOYMENT_SCAFFOLD_DIR
+>&2 export TF_VAR_scaffolding_root="${TERRAGRNT_DEPLOYMENT_DIR}/terragrunt"
 >&2 echo "terragrunt scaffold github.com/je-sidestuff/terraform-github-orchestration//modules/terragrunt/scaffolder/from-json?ref=$TGO_REF --var=InputJsonB64=/"$DEPLOY_SCAFFOLD_JSON_B64/" --terragrunt-non-interactive"
 >&2 terragrunt scaffold github.com/je-sidestuff/terraform-github-orchestration//modules/terragrunt/scaffolder/from-json?ref=$TGO_REF --var=InputJsonB64="$DEPLOY_SCAFFOLD_JSON_B64" --terragrunt-non-interactive
 >&2 terragrunt run-all apply --terragrunt-non-interactive
@@ -92,6 +93,7 @@ export TGO_REF="scaffold-refactor"
 # Scaffold our scaffolder so it can scaffold the remaining self-bootstrap tree
 >&2 mkdir -p $TERRAGRUNT_SELF_BOOTSTRAP_SCAFFOLD_DIR
 >&2 cd $TERRAGRUNT_SELF_BOOTSTRAP_SCAFFOLD_DIR
+>&2 export TF_VAR_scaffolding_root="${TERRAGRUNT_SELF_BOOTSTRAP_DIR}/terragrunt"
 >&2 echo "terragrunt scaffold github.com/je-sidestuff/terraform-github-orchestration//modules/terragrunt/scaffolder/from-json?ref=$TGO_REF --var=InputJsonB64=/"$SELF_BOOTSTRAP_SCAFFOLD_JSON_B64/" --terragrunt-non-interactive"
 >&2 terragrunt scaffold github.com/je-sidestuff/terraform-github-orchestration//modules/terragrunt/scaffolder/from-json?ref=$TGO_REF --var=InputJsonB64="$SELF_BOOTSTRAP_SCAFFOLD_JSON_B64" --terragrunt-non-interactive
 >&2 terragrunt run-all apply --terragrunt-non-interactive
